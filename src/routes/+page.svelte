8c44f1218c74598401c6117bba37b0e6b9e492ac
Sean Davis <sdavisde@gmail.com>
2023-02-20 11:32:15 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<style lang="scss">
+    .title {
+        &:hover {
+            color: red;
+        }
+    }
+</style>
+
+<h1 class='title'>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
